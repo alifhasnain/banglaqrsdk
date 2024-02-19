9960d09cf58fc4ccc8e241d0b3399ddfc16279cd
rafid <rafid.khan@sslwireless.com>
2024-02-19 14:36:21 +0600

    functionalities added

--- android/src/main/kotlin/com/banglaqr/banglaqrsdk/BanglaqrsdkPlugin.kt
+++ android/src/main/kotlin/com/banglaqr/banglaqrsdk/BanglaqrsdkPlugin.kt
@@ -6,7 +6,10 @@ import android.provider.CalendarContract.Colors
 import android.util.Log
 import androidx.annotation.NonNull
 import com.sslwireless.banglaqrsdk.model.BanglaQRInitialization
+import com.sslwireless.banglaqrsdk.model.QRResponse
+import com.sslwireless.banglaqrsdk.view.singleton.IntegrateBanglaQR
 import com.sslwireless.banglaqrsdk.view.utils.BanglaQRFundingSource
+import com.sslwireless.banglaqrsdk.viewmodel.listener.BanglaQRResponseListener
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -15,7 +18,7 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** BanglaqrPlugin */
-class BanglaqrsdkPlugin: FlutterPlugin, MethodCallHandler {
+class BanglaqrsdkPlugin: FlutterPlugin, MethodCallHandler, BanglaQRResponseListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -36,7 +39,9 @@ class BanglaqrsdkPlugin: FlutterPlugin, MethodCallHandler {
       }
       "initializeBanglaQRSdk" -> {
         Log.d("initializeBanglaQRSdk", "Initializing BanglaQR SDK")
-        BanglaQRInitialization(BanglaQRFundingSource.FundingSource.VISA, "#6200EE", "#03DAC6")
+        val banglaQR =  BanglaQRInitialization(BanglaQRFundingSource.FundingSource.VISA, "#6200EE", "#03DAC6")
+
+        IntegrateBanglaQR.getInstance(this.context).addBanglaQRInitialization(banglaQR).build(this@BanglaqrsdkPlugin)
         result.success(true)
       }
       else -> result.notImplemented()
@@ -45,5 +50,13 @@ class BanglaqrsdkPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun banglaQRFailResponse(failResponse: String) {
+    TODO("Not yet implemented")
+  }
+
+  override fun banglaQRSuccessResponse(qrResponse: QRResponse) {
+    TODO("Not yet implemented")
   }
 }
